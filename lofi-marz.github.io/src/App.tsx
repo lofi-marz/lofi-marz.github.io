5ee794037aa8b3f96b2607f41935e9d8b250b936
Omari <omari.thompson-edwards>
2022-02-25 15:21:15 +0000

    Set up linting

--- lofi-marz.github.io/src/App.tsx
+++ lofi-marz.github.io/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import me from  './images/8183486.jpeg';
+import { FaGithub, FaBook, FaAt } from 'react-icons/fa';
+import {ReactElement} from 'react';
+import {IconType} from 'react-icons';
+
+interface Icon {
+    icon: ReactElement<IconType>
+    link: string;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const icons: Icon[] = [
+        {icon: <FaGithub/>, link:'https://github.com/lofi-marz'},
+        {icon: <FaBook/>, link:'https://github.com/lofi-marz'},
+        {icon: <FaAt/>, link:'mailto:othompsonedwards@gmail.com'}
+    ];
+
+
+    return (
+        <div className='container centered-flex'>
+            <div id="main-box">
+                <img id="me" src={me}/>
+                <div className="icon-container">
+                    {icons.map((i => <a key={i.link} className='social-icon' href={i.link}>{i.icon}</a>))}
+                </div>
+                <h1 id="name" className="title">{'Hi, I\'m '}<span className="highlight">Omari</span></h1>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
