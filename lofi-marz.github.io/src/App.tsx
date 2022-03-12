319478e8351bb1c0b10f5d557b1e4bafa89f04b3
Omari <omari.thompson-edwards>
2022-03-12 11:53:01 +0000

    Redesigned website

--- lofi-marz.github.io/src/App.tsx
+++ lofi-marz.github.io/src/App.tsx
@@ -1,10 +1,8 @@
 
 import './App.css';
-import me from  './images/8183486.jpeg';
-import { FaGithub, FaBook, FaAt, FaLinkedin } from 'react-icons/fa';
+import {FaGithub, FaBook, FaAt, FaLinkedin} from 'react-icons/fa';
 import {ReactElement} from 'react';
-import {IconType} from 'react-icons';
-import MetaballsSketch from './sketches/metaballs-sketch';
+import {IconType, IconContext} from 'react-icons';
 
 interface Icon {
     icon: ReactElement<IconType>
@@ -22,19 +20,29 @@ function App() {
         {icon: <FaLinkedin/>, link:'https://www.linkedin.com/in/omari-thompson-edwards-b7307b195/'}
     ];
 
+    /*const [p5Active, setP5Active] = useState(true);
+
+    const getp5 = (): ReactElement | undefined  => {
+        if (p5Active) {
+            return <MetaballsSketch/>;
+        } else {
+            return <FaGift onClick={() => setP5Active(true)}/>;
+        }
+    };*/
 
     return (
         <div className='container centered-flex'>
-            <div id="main-box">
-                <img id="me" src={me}/>
+            <section id="main-box">
                 <div>
-                    <h1 id="name" className="title">{'Hi, I\'m '}<span className="highlight">Omari</span></h1>
-                    <div className="icon-container">
-                        {icons.map((i => <a key={i.link} className='social-icon' href={i.link}>{i.icon}</a>))}
-                    </div>
+                    <h1 id="name" className="title"><span className='bracket'>{'<'}</span>Hi, I&apos;m <span className="highlight">Omari</span><span className='bracket'>{'/>'}</span></h1>
+                    <IconContext.Provider value={{size:'2rem'}}>
+                        <div className="icon-container">
+                            {icons.map((i => <a key={i.link} className='social-icon' href={i.link}>{i.icon}</a>))}
+                        </div>
+                    </IconContext.Provider>
+
                 </div>
-            </div>
-            <MetaballsSketch></MetaballsSketch>
+            </section>
         </div>
     );
 }
--- lofi-marz.github.io/src/sketches/metaballs-sketch.tsx
+++ lofi-marz.github.io/src/sketches/metaballs-sketch.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Sketch from 'react-p5';
 import P5 from 'p5';
 
-const SCALE = 0.5;
+const SCALE = 1;
 
 class Metaball {
     x: number;
@@ -35,7 +35,7 @@ function dist(x1: number, y1: number, x2: number, y2: number) {
 }
 
 function squaredDist(x1: number, y1: number, x2: number, y2: number) {
-    return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
 }
 
 
@@ -45,77 +45,93 @@ function squaredDist(x1: number, y1: number, x2: number, y2: number) {
 const MetaballsSketch: React.FC = () => {
 
     let metaballs: Metaball[];
+
     //See annotations in JS for more information
     const setup = (p5: P5, canvasParentRef: Element) => {
 
-        p5.createCanvas(p5.windowWidth * SCALE, p5.windowHeight * SCALE).parent(canvasParentRef);
+        //p5.createCanvas(p5.windowWidth * SCALE, p5.windowHeight * SCALE).parent(canvasParentRef);
+        p5.createCanvas(500, 500).parent(canvasParentRef);
         p5.pixelDensity(1);
+
         metaballs = [];
 
-        for (let i = 0; i < 20; i++) {
+        for (let i = 0; i < 10; i++) {
             const x = p5.random(p5.width);
             const y = p5.random(p5.height);
-            const r = p5.random(50 * SCALE,150 * SCALE);
-            const vx = p5.random(-10 * SCALE, 10 * SCALE);
-            const vy = p5.random(-10 * SCALE, 10 * SCALE);
+            const r = p5.random(50 * SCALE,1000 * SCALE);
+            const vx = p5.random(-5 * SCALE, 5 * SCALE);
+            const vy = p5.random(-5 * SCALE, 5 * SCALE);
             metaballs.push(new Metaball(x,y, r, vx, vy));
         }
 
     };
 
-
-
     const draw = (p5: P5) => {
-
+        p5.colorMode(p5.HSB);
         p5.loadPixels();
-        p5.background('black');
+        p5.background('#212529');
 
-        let i = 0;
 
         //TODO: Make this more efficient
+
         for (let x = 0; x < p5.width; x++) {
             for (let y = 0; y < p5.height; y++) {
+
                 let density = 0;
-                i = 4 * (x + y * p5.width);
-                metaballs.forEach((m) => {
+
+                const i = 4 * (x + y * p5.width);
+
+                for (let j = 0; j < metaballs.length; j++){
+                    const m = metaballs[j];
 
                     //const d = dist(x,y, m.x, m.y);
 
-                    density += m.r * m.r / squaredDist(x, y, m.x, m.y);
+                    density += m.r * m.r /  squaredDist(x, y, m.x, m.y);
 
+                    //density = p5.map(density, 0, 100, 0, 255, true);
+                    //if (density > 0) console.log(density);
                     //if (Math.round(d) == m.r) density = 255;
-                });
+                }
+                //console.log(density);
                 //if (i % 100 == 0) console.log(density);
-                density = density > 50 ? 255 :  0;
+                //density = density > 5 ? 255 :  0;
+                //if (density < slider.value()) density/=
+
 
                 p5.pixels[i] = density;
                 p5.pixels[i+1] = density;
                 p5.pixels[i+2] = density;
-                p5.pixels[i+3] = 255;
+                p5.pixels[i+3] = density;
+
             }
         }
 
-        metaballs.forEach((m) => {
+        for (let i = 0; i < metaballs.length; i++){
+            const m = metaballs[i];
             m.x += m.vx;
             m.y += m.vy;
 
             if (m.x < 0)
             {
-                m.vx = m.startVx;
+                m.x -= m.vx;
+                m.vx = -m.vx;
             } else if (m.x > p5.width ) {
-                m.vx = -m.startVx;
+
+                m.x -= m.vx;
+                m.vx = -m.vx;
             }
 
             if (m.y < 0)
             {
-                m.vy = m.startVy;
+
+                m.y -= m.vy;
+                m.vy = -m.vy;
             } else if (m.y > p5.height) {
-                m.vy = -m.startVy;
+
+                m.y -= m.vy;
+                m.vy = -m.vy;
             }
-
-
-
-        });
+        }
 
         p5.updatePixels();
 
@@ -124,14 +140,14 @@ const MetaballsSketch: React.FC = () => {
     };
 
 
-
-    const onWindowResize = (p5: P5) => {
+    /*const onWindowResize = (p5: P5) => {
         p5.resizeCanvas(p5.windowWidth * SCALE, p5.windowHeight * SCALE);
-    };
+    };*/
 
 
 
-    return <Sketch setup={setup}  windowResized={onWindowResize} draw={draw} />;
+    return <Sketch setup={setup} draw={draw} />;
 };
 
 export default MetaballsSketch;
+
