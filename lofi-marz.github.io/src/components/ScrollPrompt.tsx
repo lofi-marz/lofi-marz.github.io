c0b6b52e35997bb5a16aea8c1886a79d1a4b6316
Omari <marzipant.e@gmail.com>
2022-04-04 10:10:06 +0100

    Random sketch selection not working

---
+++ lofi-marz.github.io/src/components/ScrollPrompt.tsx
@@ -0,0 +1,16 @@
+
+import {FaChevronDown} from 'react-icons/fa';
+
+import IconLink from './IconLink';
+
+
+
+
+
+const ScrollPrompt = () => {
+    return <IconLink href='#about'>
+        <FaChevronDown></FaChevronDown>
+    </IconLink>;
+};
+
+export default ScrollPrompt;
