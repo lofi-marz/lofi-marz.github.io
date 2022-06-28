207c355809cc1b20603bcca688f97ec9f6b28af6
Omari <marzipant.e@gmail.com>
2022-06-28 12:32:54 +0100

    Add profile picture

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,5 +1,7 @@
 import { FaAt, FaChevronDown, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { WithChildrenProps } from '../../types';
+import Image from 'next/image';
+import me from '../../me.jpg';
 
 function Chevron() {
     return <FaChevronDown className="absolute mt-20" />;
@@ -23,41 +25,52 @@ function IconLink({ href, children }: IconLinkProps) {
 export function About() {
     //[calc(100vh-4rem)]
     return (
-        <section
-            id="about"
-            className="flex h-screen flex-col items-center justify-center dark:bg-zinc-900 lg:w-1/2">
-            <h1 className="z-10 my-10 self-start border-l-8 border-primary pl-5 font-title text-5xl font-bold">
-                about me
-            </h1>
-            <div className="text-content prose mix-blend-difference dark:prose-invert dark:text-white">
-                Hi, I&apos;m Omari! I&apos;m a Front-End Developer from England,
-                currently studying at the{' '}
-                <a
-                    href="https://www.nottinghampost.com/news/nottingham-news/picture-shows-huge-gaggle-geese-3383819"
-                    className="link">
-                    University of Nottingham
-                </a>
-                {'. '}I like making fun,{' '}
-                <a
-                    className="link"
-                    href="https://p5js-experiments-gamma.vercel.app/">
-                    creative
-                </a>{' '}
-                things with code.
-                <p className="mx-auto w-fit">
-                    Feel free to contact me at any of these!
-                    <span className="mx-auto flex w-min gap-10 px-10 py-5">
-                        <IconLink href="https://github.com/lofi-marz/">
-                            <FaGithub />
-                        </IconLink>
-                        <IconLink href="https://www.linkedin.com/in/omari-thompson-edwards-b7307b195/">
-                            <FaLinkedin />
-                        </IconLink>
-                        <IconLink href="mailto:othompsonedwards@gmail.com">
-                            <FaAt />
-                        </IconLink>
-                    </span>
-                </p>
+        <section className="flex h-screen flex-col-reverse md:flex-row">
+            <div
+                id="about"
+                className="flex flex-col items-center justify-center dark:bg-zinc-900 md:w-1/2 lg:h-screen">
+                <h1 className="z-10 my-10 self-start border-l-8 border-primary pl-5 font-title text-5xl font-bold">
+                    about me
+                </h1>
+                <div className="text-content prose mix-blend-difference dark:prose-invert dark:text-white">
+                    Hi, I&apos;m Omari! I&apos;m a Front-End Developer from
+                    England, currently studying at the{' '}
+                    <a
+                        href="https://www.nottinghampost.com/news/nottingham-news/picture-shows-huge-gaggle-geese-3383819"
+                        className="link">
+                        University of Nottingham
+                    </a>
+                    {'. '}I like making fun,{' '}
+                    <a
+                        className="link"
+                        href="https://p5js-experiments-gamma.vercel.app/">
+                        creative
+                    </a>{' '}
+                    things with code.
+                    <p className="mx-auto w-fit">
+                        Feel free to contact me at any of these!
+                        <span className="mx-auto flex w-min gap-10 px-10 py-5">
+                            <IconLink href="https://github.com/lofi-marz/">
+                                <FaGithub />
+                            </IconLink>
+                            <IconLink href="https://www.linkedin.com/in/omari-thompson-edwards-b7307b195/">
+                                <FaLinkedin />
+                            </IconLink>
+                            <IconLink href="mailto:othompsonedwards@gmail.com">
+                                <FaAt />
+                            </IconLink>
+                        </span>
+                    </p>
+                </div>
+            </div>
+            <div className="flex w-full items-center md:h-full md:w-1/2">
+                <div className="m-auto border-l-8 border-primary pl-3 md:w-96 md:max-w-[50vw]">
+                    <img
+                        className="saturate-90"
+                        src="me.jpg"
+                        alt="Picture of me"
+                    />
+                </div>
             </div>
         </section>
     );
