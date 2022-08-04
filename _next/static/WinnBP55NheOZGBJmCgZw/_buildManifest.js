2ff587d260a8a4495ded971c974e48b8a8010bf9
Omari <marzipant.e@gmail.com>
2022-08-04 14:38:40 +0100

    Updates

--- _next/static/WinnBP55NheOZGBJmCgZw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-3fbf4e18db19da97.js","static/chunks/pages/index-6ac4400ffe00ed0c.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
