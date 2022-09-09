8824a7ed4a46e55ff9c9755d1db6ab3d5985e6de
lofi-marz <marzipant.e@gmail.com>
2022-09-09 08:41:58 +0100

    Updates

--- _next/static/BroVTdi6ErWU_V6qa-cIs/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-68ba0b0bbc722aa0.js","static/chunks/148-55f11ce613195fd2.js","static/chunks/pages/index-ed7b9a44a7968ca2.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/SDCv9HW4iJGh7UIvoY6JW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-68ba0b0bbc722aa0.js","static/chunks/962-4c0047cada57ea02.js","static/chunks/pages/index-8860eff2e84b02c1.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
