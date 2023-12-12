fbaff5fdaf370b18bdf9d6d3b2443d6750c59b22
Omari <marzipant.e@gmail.com>
2023-12-12 10:39:36 +0000

    Updates

---
+++ _next/static/X84gPb2lbFXuWXFm4lIIU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-46d258992f85f540.js","static/chunks/18-b53f07c4fc360365.js","static/chunks/pages/index-95ec8d767bd99548.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
